6fb72d8052ff0555a31ce838fbf23c16f0c129d5
Kay Watanabe <kay.watanabe80@gmail.com>
2019-11-06 01:38:41 -0800

    Move .cta-btn default on body, set up top jump btn

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 // Responsive Menu - Dropdown
 const body = document.body;
 const hamburger = document.getElementById('hamburger');
+const gotoTop = document.getElementById('gotopbtn');
 
 hamburger.addEventListener('click', openMenu);
 hamburger.addEventListener('mousedown', function(e){
@@ -10,3 +11,29 @@ hamburger.addEventListener('mousedown', function(e){
 function openMenu(){
     body.classList.toggle('show');
 }
+
+// Show / Hide Menu
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    gotoTop.style.visibility = 'visible';
+  } else {
+    gotoTop.style.visibility ='hidden';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+  
+  //Smooth Scroll
+ const scroll = new SmoothScroll('a[href*="#"]',  {
+      offset: 20,
+      speed:500,
+      speedAsDuration: true,
+      durationMax:500
+  });
