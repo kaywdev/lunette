820c11f953032b46c30d0e588df5d5dad441f62f
Kay Watanabe <kay.watanabe80@gmail.com>
2019-11-13 08:55:20 -0800

    Add slick slider setup

--- scripts/script.js
+++ scripts/script.js
@@ -52,3 +52,33 @@ function topFunction() {
       durationMax:500
   });
 
+  //slides js
+  $(document).ready(function(){
+    $('.style-gallery').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  });
+
