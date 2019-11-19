0d18671377941f76b180f8f115b76a35acb9f66c
Kyungmi Moon <kyungmimoondesign@gmail.com>
2019-11-19 01:56:07 -0800

    Add function of chaning colors based on lense / frame

--- scripts/spinner.js
+++ scripts/spinner.js
@@ -1,3 +1,5 @@
+// Code modified from Michael Whyte's 
+// Shopping Cart - v1.0
 
 // HTML Elements
 // Slide Elements
@@ -14,50 +16,182 @@ const $selectedFrameOut = $('#selected-frame-out');
 
 
 
-// Lense Checkbox
+
+var lense = document.querySelector('input[name="lense-color"]:checked').value;
+var frame = document.querySelector('input[name="frame-color"]:checked').value;
+
+
 $checkboxLense.on('change', function () {
+    lense = $(this).val();
+    console.log(lense, frame)
+    $slide.attr('href', `images/product-item/sg/sg_${frame}_${lense}_1.png`);
+    $slide.attr({
+        src: `images/product-item/sg/sg_${frame}_${lense}_1.png`,
+        alt: `${frame} ${lense} sunglasses`
+    });
 
-    // Grab the current src attribute value of the 
-    // main glasses slide image
-    const currentSrc = $slide.attr('src');
-
-    // Grab the value of the radio button that was
-    // checked...
-    const lense = $(this).val();
-    let currentLense = lense;
-
-
-    // Update the lense output
+    // Update the color output
     $selectedLenseOut.text(capitalizeFirstLetter(lense));
-    console.log(currentLense);
 });
 
 
 
-// // Frame Checkbox
-// $checkboxFrame.on('change', function () {
+$checkboxFrame.on('change', function () {
+    frame = $(this).val();
+    console.log(lense, frame)
+    $slide.attr('href', `images/product-item/sg/sg_${frame}_${lense}_1.png`);
+    $slide.attr({
+        src: `images/product-item/sg/sg_${frame}_${lense}_1.png`,
+        alt: `${frame} ${lense} sunglasses`
+    });
 
-//     // Grab the value of the radio button that was
-//     // checked...
-//     const frame = $(this).val();
-//     let currentFrame = frame;
-//     console.log(currentFrame);
-
-//     $btnAddToCart.val('Add to Cart')
-//                  .removeAttr('disabled');
-
-//      // Update the lense output
-//     $selectedFrameOut.text(capitalizeFirstLetter(frame));
-    
-// });
+    // Update the color output
+    $selectedFrameOut.text(capitalizeFirstLetter(frame));
+});
 
 
-// // Image output
-// $slide.attr('href', `images/product-item/sg/sg_${currentFrame}_${currentLense}_1.png`);
-// $slide.attr({
-//     src: `images/product-item/sg/sg_${currentFrame}_${currentLense}_1.png`,
-//     alt: `${currentFrame} ${currentLense} sunglasses`
-// });
+
+// ------------------------- TEST 1 ---------------------------
+// // Current checked Radio button - Lense color
+// const lenseRadios = document.getElementsByName('lense-color');
+// function findLense(){
+//     for (var i = 0, length = lenseRadios.length; i < length; i++) {
+//         if (lenseRadios[i].checked) {
+//             const currentLense = lenseRadios[i].value;
+//             alert(currentLense);
+            
+//         }
+//     };
+// };
+
+// // Current checked Radio button - Frame color
+// const frameRadios = document.getElementsByName('frame-color');
+// function findFrame(){
+//     for (var i = 0, length = frameRadios.length; i < length; i++) {
+//         if (frameRadios[i].checked) {
+//             const currentFrame = frameRadios[i].value;
+//             alert(currentFrame);
+
+//         }
+//     };
+// };
+
+/*
+$checkboxLense.on('change', function () {
+    findLense();
+});
+
+
+
+$checkboxFrame.on('change', function () {
+    findFrame();
+});
+
+
+
+
+// ------------------------- TEST 2 ---------------------------
+/*
+(function () {
+    var radios = document.getElementsByName('lense-color');
+    var radios2 = document.getElementsByName('frame-color');
+
+    for (var i = 0; i < radios.length; i++) {
+        radios[i].onclick = function () {
+            // console.log(this.value);
+            const currentLense = this.value;
+            // console.log(currentLense);
+        }
+    }
+    for (var i = 0; i < radios2.length; i++) {
+        radios2[i].onclick = function () {
+            // console.log(this.value);
+            const currentFrame = this.value;
+            // console.log(currentFrame);
+        }
+    }
+    console.log(currentFrame);
+    // console.log(${ currentFrame } ${ currentLense } sunglasses)
+})();
+*/
+
+
+// ------------------------- TEST 3..failed ---------------------------
+/*
+$checkboxLense.on('change', function () {
+    $('input[name=lense-color], input[name=frame-color]').change(function () {
+        // $('#image').prop('src', $('input[name=lense-color]:checked').val() + '-' + $('input[name=rframe-color]:checked').val() + '.jpg');
+        $('#image').prop('src', `images/produt-item/sg/sg_${('input[name=lense-color]:checked').val()} + '-' 
+        + ${('input[name=rframe-color]:checked').val()} + '.jpg'`);
+    };
+});
+*/
+
+
+
+/*
+function combineLenseFrame(currentLense, currentFrame) {
+    $slide.attr('href', `images/product-item/sg/sg_${currentFrame}_${currentLense}_1.png`);
+    $slide.attr({
+        src: `images/product-item/sg/sg_${currentFrame}_${currentLense}_1.png`,
+        alt: `${currentFrame} ${currentLense} sunglasses`
+    });
+}
+*/
+
+
+
+/*
+// Grab the current src attribute value of the 
+// main glasses slide image
+const currentSrc = $slide.attr('src');
+// Grab the value of the radio button that was
+// checked...
+const currentLense = $checkboxLense('change').attr('value'); 
+// const currentLense = $(this).val();
+// const currentFrame = $()
+// const lense = $(this).val();
+console.log(currentLense);
+
+const currentFrame = 
+
+$slide.attr('href', `images/product-item/sg/sg_${currentFrame}_${currentLense}_1.png`);
+$slide.attr({
+    src: `images/product-item/sg/sg_${currentFrame}_${currentLense}_1.png`,
+    alt: `${currentFrame} ${currentLense} sunglasses`
+});
+
+// Update the color output
+$selectedLenseOut.text(capitalizeFirstLetter(lense));
+
+*/
+
+
+
+/*
+// Frame Checkbox
+$checkboxFrame.on('change', function () {
+
+    // Grab the value of the radio button that was
+    // checked...
+    const frame = $(this).val();
+    console.log(this);
+
+    $slide.attr('href', `images/product-item/sg/sg_rosegold_${lense}_1.png`);
+    $slide.attr({
+        src: `images/product-item/sg/sg_rosegold_${lense}_1.png`,
+        alt: `rosegold ${lense} sunglasses`
+    });
+
+
+
+    $btnAddToCart.val('Add to Cart')
+        .removeAttr('disabled');
+    $selectedFrameOut.text(capitalizeFirstLetter(frame));
+
+});
+
+*/
 
 
 // Utility Functions
