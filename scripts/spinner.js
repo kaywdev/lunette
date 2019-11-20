cb1148f07201070bd0a810ea45ca3a46006be269
Kyungmi Moon <kyungmimoondesign@gmail.com>
2019-11-20 14:50:03 -0800

    Fix rotating problem

--- scripts/spinner.js
+++ scripts/spinner.js
@@ -13,20 +13,20 @@ const $btnAddToCart = $('#btn-add-to-cart');
 // Selected Product Information Output Elements
 const $selectedLenseOut = $('#selected-lense-out');
 const $selectedFrameOut = $('#selected-frame-out');
-
-
+// Range slider  
+const $rangeSlider = $("#myRange");
 
 
 let lense = document.querySelector('input[name="lense-color"]:checked').value;
 let frame = document.querySelector('input[name="frame-color"]:checked').value;
-
+let fileNum = document.getElementById('myRange').value;
 
 $checkboxLense.on('change', function () {
     lense = $(this).val();
-    console.log(lense, frame)
-    $slide.attr('href', `images/product-item/sg/sg_${frame}_${lense}_1.png`);
+    // console.log(lense, frame)
+    $slide.attr('href', `images/product-item/sg/sg_${frame}_${lense}_${fileNum}.png`);
     $slide.attr({
-        src: `images/product-item/sg/sg_${frame}_${lense}_1.png`,
+        src: `images/product-item/sg/sg_${frame}_${lense}_${fileNum}.png`,
         alt: `${frame} ${lense} sunglasses`
     });
 
@@ -37,10 +37,10 @@ $checkboxLense.on('change', function () {
 
 $checkboxFrame.on('change', function () {
     frame = $(this).val();
-    console.log(lense, frame)
-    $slide.attr('href', `images/product-item/sg/sg_${frame}_${lense}_1.png`);
+    // console.log(lense, frame)
+    $slide.attr('href', `images/product-item/sg/sg_${frame}_${lense}_${fileNum}.png`);
     $slide.attr({
-        src: `images/product-item/sg/sg_${frame}_${lense}_1.png`,
+        src: `images/product-item/sg/sg_${frame}_${lense}_${fileNum}.png`,
         alt: `${frame} ${lense} sunglasses`
     });
 
@@ -50,18 +50,20 @@ $checkboxFrame.on('change', function () {
 
 
 
-// Range slider from W3C School 
-let rangeSlider = document.getElementById("myRange");
-
+// Range slider function
 // Update the current slider value (each time you drag the slider handle)
-rangeSlider.oninput = function () {
-    console.log($(this).val());
-}
-
+$rangeSlider.on('input', function () {
+    // console.log($(this).val());
+    fileNum = $(this).val();
+    $slide.attr('href', `images/product-item/sg/sg_${frame}_${lense}_${fileNum}.png`);
+    $slide.attr({
+        src: `images/product-item/sg/sg_${frame}_${lense}_${fileNum}.png`,
+        alt: `${frame} ${lense} sunglasses`
+    });
+});
 
 
 // Utility Functions
-
 // Capitalizes the first character in a string
 // Code modified from this stackoverflow quesion:
 // https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
