e335306d8d19ac63d08f80a059082d857480fc21
Kay Watanabe <kay.watanabe80@gmail.com>
2019-11-20 22:27:40 -0800

    Fixed link

--- scripts/script.js
+++ scripts/script.js
@@ -52,33 +52,4 @@ function topFunction() {
       durationMax:500
   });
 
-  //slides js
-  $(document).ready(function(){
-    $('.style-gallery').slick({
-      dots: true,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 900,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-  });
-
+ 
